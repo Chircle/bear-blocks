53adeb141b6316ab48a05cd0c54b18702d7db690
Simert,Mechu <Mechu.Simert@adesso.de>
2025-12-12 15:07:08 +0100

    remaing base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/bear-blocks/',
+    base: process.env.NODE_ENV === 'production' ? '/bear-blocks/' : '/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
